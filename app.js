91cbb53d352e82122f040b78247397e10b2d2a96
KotaroYamazaki <sentoryoku53m@gmail.com>
2022-06-08 20:19:48 +0900

    .env からfirebase の設定を読み取るように修正

--- app.js
+++ app.js
@@ -4,22 +4,19 @@
 const express = require('express');
 const app = express();
 const session = require('express-session');
-const bcrypt = require('bcrypt');
+require('dotenv').config();
 
 const  { initializeApp } = require('firebase/app');
 const { getFirestore, collection, getDocs } = require('firebase/firestore/lite');
 
-// Follow this pattern to import other Firebase services
-// import { } from 'firebase/<service>';
-
-// TODO: Replace the following with your app's Firebase project configuration
 const firebaseConfig = {
-        apiKey: x,
-        authDomain: "t0cre8.firebaseapp.com",
-        databaseURL: "https://t0cre8.firebaseio.com",
-        storageBucket: "t0cre8.appspot.com",
-        messagingSenderId: "1027348099985",
-        projectId: "t0cre8"
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
+  measurementId: process.env.FIREBASE_MEASUREMENT_ID,
 };
 
 const firebaseapp = initializeApp(firebaseConfig);
