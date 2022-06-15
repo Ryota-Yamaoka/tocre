f9d897066debe0241a40d02f054bbefc9b230ad7
Ryota-Yamaoka <t21772ry@sfc.keio.ac.jp>
2022-06-16 07:36:07 +0900

    psql

--- app.js
+++ app.js
@@ -5,14 +5,33 @@ const app = express();
 const session = require("express-session");
 const bodyParser = require("body-parser");
 const firebaseConfig = {
-  apiKey: "x",
-  authDomain: "x",
-  projectId: "x",
-  storageBucket: "x",
-  messagingSenderId: "x",
-  appId: "x",
-  measurementId: "x",
+  apiKey: "AIzaSyAxetUkclvXWGL9ZvYKoGfnxWbtAmYcHg0",
+  authDomain: "t0cre8.firebaseapp.com",
+  projectId: "t0cre8",
+  storageBucket: "t0cre8.appspot.com",
+  messagingSenderId: "1027348099985",
+  appId: "1:1027348099985:web:9e5a0eda2c76776e89a030",
+  measurementId: "G-LCWJQD8GEF",
 };
+const pgp = require("pg-promise")(/* options */);
+const connection = {
+  host: "localhost",
+  port: 5432,
+  database: "tocre",
+  user: "postgres",
+  password: "",
+};
+const db = pgp(connection);
+
+app.get('/users/:userId', (req, res) => {
+  db.one("SELECT * FROM users WHERE id=$1", [req.params.userId])
+  .then((data) => {
+    res.send(data);
+  })
+  .catch((error) => {
+    console.log("ERROR:", error);
+  });
+})
 
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: false }));
