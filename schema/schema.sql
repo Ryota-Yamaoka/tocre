fd916e824c3e4249e7bd5e0157204f6b5de6ed94
Ryota-Yamaoka <t21772ry@sfc.keio.ac.jp>
2022-06-18 07:45:48 +0900

    createworkstable

--- schema/schema.sql
+++ schema/schema.sql
@@ -6,3 +6,17 @@ CREATE TABLE users (
   updated_at TIMESTAMP WITH TIME ZONE NOT NULL default current_timestamp,
   PRIMARY KEY (id)
 );
+
+CREATE TABLE works (
+  id SERIAL NOT NULL,
+  user_id REFERENCES users (id) NOT NULL,
+  url VARCHAR(2048) NOT NULL,
+
+  :  -- 必要なカラムを追加してみてください。
+　　　　　　　　　　　　　文字列の場合の型はVARCHAR(255 or 512 or 1024 or ..) になります。
+　　　　　　　　　　　　　中の数字は文字列の想定の長さによって使い分けます。日本語１文字がVARCHAR2ぐらいのイメージです
+
+  created_at TIMESTAMP WITH TIME ZONE NOT NULL default current_timestamp,
+  updated_at TIMESTAMP WITH TIME ZONE NOT NULL default current_timestamp,
+  PRIMARY KEY (id)
+);
